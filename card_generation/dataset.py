4e58dbfbb46c917ff8d3a9dcc5201556a16601bd
mert <46087170+peryngveohlin666@users.noreply.github.com>
2021-01-16 23:31:04 +0000

    need to fix a bunch of crap, also rip 3080.

--- card_generation/dataset.py
+++ card_generation/dataset.py
@@ -1,3 +1,19 @@
+# This file is part of the tashimasu distribution (https://github.com/peryngveohlin666/tashimasu).
+# Copyright (c) 2021 peryngveohlin666.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 import os
 import queue
 import threading
@@ -7,7 +23,6 @@ from PIL import Image
 import numpy as np
 
 path = "../dataset"
-thread_num = 16
 
 
 
@@ -24,12 +39,12 @@ def load_data(files):
     return x, y
 
 # load the whole dataset running the load data function in multiple threads
-def load_dataset(load_from, load_to):
+def load_dataset(load_from, load_to, thread_num):
     x = []
     y = []
     threads = []
     # split the data into chunks equal to the thread count (to avoid memory errors I am splitting the dataset as it is too big for my memory to handle)
-    chunks = np.split(np.asarray(os.listdir(path)[load_from:load_to]), thread_num)
+    chunks = np.array_split(np.asarray(os.listdir(path)[load_from:load_to]), thread_num)
     with concurrent.futures.ThreadPoolExecutor() as executor:
         for chunk in chunks:
             f = executor.submit(load_data, chunk)
@@ -40,7 +55,3 @@ def load_dataset(load_from, load_to):
         for Y in thread.result()[1]:
             y.append(Y)
     return np.asarray(x), np.asarray(y)
-
-
-
-print(load_dataset(0, 6400))
---
+++ card_generation/gan.py
@@ -0,0 +1,144 @@
+import os
+
+from keras.layers import Input, Dense, Reshape, Flatten
+from keras.layers import BatchNormalization
+from keras.layers.advanced_activations import LeakyReLU
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+import numpy as np
+import dataset as ds
+from keras.preprocessing.image import array_to_img
+
+img_rows = 512
+img_cols = 512
+channels = 3
+img_shape = (img_rows, img_cols, channels)
+
+def build_generator():
+    noise_shape = (100,)  # 1D array of size 100 (latent vector / noise)
+
+    model = Sequential()
+
+    model.add(Dense(64, input_shape=noise_shape))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(256))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+
+    model.add(Dense(np.prod(img_shape), activation='tanh'))
+    model.add(Reshape(img_shape))
+
+    model.summary()
+
+    noise = Input(shape=noise_shape)
+    img = model(noise)  # Generated image
+
+    return Model(noise, img)
+
+
+def build_discriminator():
+    model = Sequential()
+
+    model.add(Flatten(input_shape=img_shape))
+    model.add(Dense(256))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dense(1, activation='sigmoid'))
+    model.summary()
+
+    img = Input(shape=img_shape)
+    validity = model(img)
+
+    return Model(img, validity)
+
+
+def train(epochs, batch_size=128, save_interval=50, ds_begin=0, ds_end=1600, threads=16):
+    # Load the dataset
+    X_train = ds.load_dataset(ds_begin, ds_end, threads)[0]
+
+    # Convert to float and Rescale -1 to 1 (Can also do 0 to 1)
+    X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+
+    half_batch = int(batch_size / 2)
+
+    for epoch in range(epochs):
+
+
+        idx = np.random.randint(0, X_train.shape[0], half_batch)
+        imgs = X_train[idx]
+
+        noise = np.random.normal(0, 1, (half_batch, 100))
+
+        gen_imgs = generator.predict(noise)
+
+        d_loss_real = discriminator.train_on_batch(imgs, np.ones((half_batch, 1)))
+        d_loss_fake = discriminator.train_on_batch(gen_imgs, np.zeros((half_batch, 1)))
+
+        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+        noise = np.random.normal(0, 1, (batch_size, 100))
+
+        valid_y = np.array([1] * batch_size)
+
+        g_loss = combined.train_on_batch(noise, valid_y)
+
+        print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
+
+        if epoch % save_interval == 0:
+            save_imgs(epoch, "" + str(ds_begin) + "-" + str(ds_end))
+
+
+def save_imgs(epoch, ds):
+    noise = np.random.normal(0, 1, (1, 100))
+    gen_imgs = generator.predict(noise)
+    for image in gen_imgs:
+        img = array_to_img(image)
+        img.save("output/" + ds + "_anime" + ".png")
+
+
+optimizer = Adam(0.0002, 0.5)
+
+discriminator = build_discriminator()
+discriminator.compile(loss='binary_crossentropy',
+                      optimizer=optimizer,
+                      metrics=['accuracy'])
+
+generator = build_generator()
+generator.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+
+z = Input(shape=(100,))
+img = generator(z)
+
+
+discriminator.trainable = False
+
+
+valid = discriminator(img)
+
+combined = Model(z, valid)
+combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+for i in range(0, 140000, 256):
+    train(epochs=50, batch_size=256, save_interval=50, ds_begin=i, ds_end=i+256)
+    generator.save('generator_model.h5')
