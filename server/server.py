a502dd71fb5801b6dabdfbe535c91c3dc82fa40e
peryngveohlin666 <46087170+peryngveohlin666@users.noreply.github.com>
2021-01-31 00:49:08 +0000

    figured some cool stuff so took notes, small improvements

--- server/server.py
+++ server/server.py
@@ -1,19 +1,50 @@
-import asyncio
-import websockets
+import asyncio, websockets
 
-import asyncio
-import websockets
+IP = "localhost"
+PORT = 6565
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
+SEPERATOR = ":"    # seperate the protocol level messages like LOGIN and the data that comes with them
+DATA_SEPERATOR = ","    # seperate the data sent alongside with a message
 
-    greeting = f"Hellooooooo {name}!"
+LOGIN_MESSAGE = "LOGIN"
 
-    await websocket.send(greeting)
-    print(f"> {greeting}")
+counter = 0
 
-start_server = websockets.serve(hello, "localhost", 6565)
+
+def handle_message(message):
+    if message == "connected":
+        response = "ayyyyyyyy"
+    else:
+        response = f"h{message}"
+
+    return response
+
+
+def login(username, password):
+    if username == password:
+        return True
+    return False
+
+
+protocol_to_function = {
+        "LOGIN": login
+    }
+
+
+async def respond(websocket, path):
+    i = 0 # this information is different for each client
+    try:
+        async for message in websocket:
+            i+=1
+            message = message.decode('UTF-8')
+            response = handle_message(message)
+            response = str(i)
+            print(i)
+            await websocket.send(response)
+    finally:
+        print(f"disconnected unresponsive socket {websocket}")
+
+start_server = websockets.serve(respond, IP, PORT)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
